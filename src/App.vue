dc8887f3c533569ebf04dfca076c6cdd935b3117
xuebin.liu <xuebin.liu@zhaopin.com.cn>
2021-03-13 11:34:26 +0800

    去除背景图blur效果

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ body::before {
   width: 100vw;
   height: 100vh;
   background: transparent url(./assets/bg.png) center center/cover no-repeat;
-  filter: blur(5px) brightness(60%);
+  // filter: blur(5px) brightness(60%);
   z-index: -1;
 }
 #app {
