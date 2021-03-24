601d5a3e91b147736af06990c1c704dd65fa0223
xuebin.liu <xuebin.liu@zhaopin.com.cn>
2021-03-24 16:18:59 +0800

    error log

--- src/main.js
+++ src/main.js
@@ -1,26 +1,10 @@
-import { createApp, reactive } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import Antd, { message } from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
-import db from './common/db'
-
-const app = createApp(App)
-
-app.use(store)
-app.use(router)
-app.use(Antd)
-
-app.mount('#app')
-
-window.globalData = reactive({
-  userInfo: db
-    .read()
-    .get('userInfo')
-    .value()
-})
 
 const updateMessageKey = 'update'
 window.ipcRenderer.on('update', (e, { event, msg, info }) => {
@@ -52,23 +36,23 @@ window.onerror = function errorHandler(msg, url, lineNo, columnNo, error) {
   Error对象: `,
     error
   )
-  const string = msg.toLowerCase()
-  const substring = 'script error'
-  if (string.indexOf(substring) > -1) {
-    // eslint-disable-next-line no-alert
-    // alert('Script Error: See Browser Console for Detail')
-  } else {
-    const description = `Message: ${msg}
-      URL: ${url}
-      Line: ${lineNo}
-      Column: ${columnNo}
-      Error object: ${JSON.stringify(error)}`
+  // const string = msg.toLowerCase()
+  // const substring = 'script error'
+  // if (string.indexOf(substring) > -1) {
+  //   // eslint-disable-next-line no-alert
+  //   // alert('Script Error: See Browser Console for Detail')
+  // } else {
+  //   const description = `Message: ${msg}
+  //     URL: ${url}
+  //     Line: ${lineNo}
+  //     Column: ${columnNo}
+  //     Error object: ${JSON.stringify(error)}`
 
-    // notification.error({
-    //   message: '出错了，请稍后重试',
-    //   description
-    // })
-  }
+  //   notification.error({
+  //     message: '出错了，请稍后重试',
+  //     description
+  //   })
+  // }
   return true
 }
 // #endregion
@@ -84,33 +68,39 @@ window.addEventListener(
 )
 // #endregion
 
-// // #region 捕获没有 Catch 的 Promise 异常
-// window.addEventListener('unhandledrejection', e => {
-//   e.preventDefault()
-//   console.error(
-//     `[LOG]: unhandledrejection -> 捕获到异常:
-//   错误信息: ${e.reason}
-//   Error对象: `,
-//     e
-//   )
-//   return true
-// })
-// // #endregion
+// #region 捕获没有 Catch 的 Promise 异常
+window.addEventListener('unhandledrejection', e => {
+  e.preventDefault()
+  console.error(
+    `[LOG]: unhandledrejection -> 捕获到异常:
+  错误信息: ${e.reason}
+  Error对象: `,
+    e
+  )
+  return true
+})
+// #endregion
 
-// // #region VUE errorHandler 和 React componentDidCatch
-// Vue.config.errorHandler = (error, vm, info) => {
-//   try {
-//     const vmName = vm && vm.$options && vm.$options.name
-//     const vmTag = vm && vm.$vnode && vm.$vnode.tag
-//     // eslint-disable-next-line no-underscore-dangle
-//     const vmUid = vm && vm._uid
-//     const description = `[Vue渲染错误]：组件为 name: ${vmName}，tag: ${vmTag}, uid: ${vmUid}，位置为 ${info}。`
-//     console.error(description)
-//     if (typeof window === 'undefined' || !window.zpStat || !window.zpStat.error) return
-//     window.zpStat.error(error, description) // 神策埋点
-//   } catch (err) {
-//     // eslint-disable-next-line no-console
-//     console.error('[Vue渲染错误]处理失败')
-//   }
-// }
-// // #endregion
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+app.use(Antd)
+
+app.mount('#app')
+
+// #region VUE errorHandler 和 React componentDidCatch
+app.config.errorHandler = (error, vm, info) => {
+  try {
+    const vmName = vm && vm.$options && vm.$options.name
+    const vmTag = vm && vm.$vnode && vm.$vnode.tag
+    // eslint-disable-next-line no-underscore-dangle
+    const vmUid = vm && vm._uid
+    const description = `[Vue渲染错误]：组件为 name: ${vmName}，tag: ${vmTag}, uid: ${vmUid}，位置为 ${info}。`
+    console.error(description)
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('[Vue渲染错误]处理失败')
+  }
+}
+// #endregion
